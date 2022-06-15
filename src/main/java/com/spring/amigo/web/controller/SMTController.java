41e920f116b580dde51d28ee1eb2792afe4e7dde
hohwanm1234 <hohwanm@gmail.com>
2022-06-15 17:25:20 +0900

    .

--- src/main/java/com/spring/amigo/web/controller/SMTController.java
+++ src/main/java/com/spring/amigo/web/controller/SMTController.java
@@ -4,6 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 2022-06-15
+ * SpringMessageTest - 삭제예정
+ * @author 문호환
+ * @param
+ * @return model
+ *
+ */
+
 @Controller
 public class SMTController {
 
