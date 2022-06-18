bde2db93f0aaf056fcb753f45ebae20270bc0c15
mjh2550 <moomjh94@naver.com>
2022-06-18 15:20:38 +0900

    mysql setting

---
+++ src/main/resources/sql/test.sql
@@ -0,0 +1,10 @@
+CREATE TABLE test_tb (
+                     id INT PRIMARY KEY AUTO_INCREMENT,
+                     name VARCHAR(32)
+                    );
+
+INSERT INTO test_tb
+            values (
+                       null
+                   , 'test1'
+                   );
