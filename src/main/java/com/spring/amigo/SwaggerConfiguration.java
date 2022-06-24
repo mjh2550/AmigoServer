d27dab3c328dd1b01c7d7d3faf9095640d137a23
mjh2550 <moomjh94@naver.com>
2022-06-24 12:17:43 +0900

    2022-06-24 / mjh / Swagger Setting

---
+++ src/main/java/com/spring/amigo/SwaggerConfiguration.java
@@ -0,0 +1,53 @@
+package com.spring.amigo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfiguration {
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder()
+                .title("Amigo Api Description")
+                .description("Amigo App / Web api Description").build();
+    }
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.spring.amigo.mobile.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+}
--- src/main/java/com/spring/amigo/mobile/controller/MobileApiController.java
+++ src/main/java/com/spring/amigo/mobile/controller/MobileApiController.java
@@ -2,14 +2,19 @@ package com.spring.amigo.mobile.controller;
 
 import com.spring.amigo.mobile.service.api.MobileApiService;
 import com.spring.amigo.model.TestVO;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-@Controller
+@ApiOperation("api test")
+@RestController
 public class MobileApiController {
 
     private final MobileApiService mobileApiService;
@@ -18,8 +23,14 @@ public class MobileApiController {
         this.mobileApiService = mobileApiService;
     }
 
+
     @ResponseBody
-    @GetMapping("mobile/api/getAllList.do")
+    @ApiOperation(value="value", notes="notes. 메모는 여기로")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "API 정상 작동"),
+            @ApiResponse(code = 500, message = "서버 에러")
+    })
+    @GetMapping("/mobile/v1/api/getAllList.do")
     public ArrayList<TestVO> getAllList(){
         return mobileApiService.getAllList();
     }
