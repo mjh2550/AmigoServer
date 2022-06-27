4fea657e4936740097e7bb809c95ea50c58d960b
hohwanm1234 <hohwanm@gmail.com>
2022-06-27 14:17:23 +0900

    2022-06-27 / 문호환 기본적인 Template 적용 및 내용 개요 정립

--- src/test/java/com/spring/amigo/codetest.java
+++ src/test/java/com/spring/amigo/codetest.java
@@ -3,46 +3,73 @@ package com.spring.amigo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 
-import java.io.*;
-import java.util.Scanner;
-
-import static java.lang.Math.min;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class codetest {
     /*
-     * 2022-06-14
+     * 2022-06-18
+     * 미로 탐색 (BFS - 너비 우선 탐색)
      * @Author 문호환 (Main)
-     * @param
      * @return
      */
-    static long L;
-    @Test
-    public void main(String[] args){
-        Scanner sc =new Scanner(System.in);
-        int W = sc.nextInt();
-        int H = sc.nextInt();
-        int f = sc.nextInt();
-        int c = sc.nextInt();
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
+    static int[][] map; // 기본 행렬
+    static int n,m; // 행,렬
+    static boolean[][] arrived; // 방문 체크 여부
+    static int[] dx = {1,0,-1,0}; // x축
+    static int[] dy = {0,1,0,-1}; // y축
 
-        L = (x2 - x1) * (y2 - y1) * (c + 1);
-        if (f <= W / 2) {
-            if (f <= x1) {
-                System.out.println(W * H - L);
-            }
-            else {
-                System.out.println(W * H - (L + (min(f, x2) - x1) * (y2 - y1) * (c + 1)));
+    public void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        arrived = new boolean[n][m];
+        arrived[0][0] = true;
+
+        // 행렬 구조 생성
+        for(int i=0; i<n; i++){
+            String str = br.readLine();
+            for(int j=0; j<m; j++){
+                map[i][j] = str.charAt(j) - '0';
             }
         }
-        else {
-            if (W <= x1+f) {
-                System.out.println(W * H - L);
-            }
-            else {
-                System.out.println(W * H - (L + (min(W, f + x2) - (f + x1)) * (y2 - y1) * (c + 1)));
+
+        search(0, 0);
+        System.out.println(map [n-1][m-1]);
+    }
+
+    /*
+     * 2022-06-18
+     * 행렬 체크
+     * @Author 문호환
+     * @param x, y
+     * @return
+     */
+
+    public static void search(int x, int y){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {x,y});
+
+        while(!queue.isEmpty()) {
+            int[] xy = queue.poll();
+
+            for(int i=0; i<4; i++) {
+                int nextX = xy[0]+dx[i];
+                int nextY = xy[1]+dy[i];
+
+                if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= m || arrived[nextX][nextY] || map[nextX][nextY] == 0) {
+                    continue;
+                }
+                queue.add(new int[] {nextX, nextY});
+                arrived[nextX][nextY] = true;
+                map[nextX][nextY] = map[xy[0]][xy[1]] + 1;
             }
         }
     }
